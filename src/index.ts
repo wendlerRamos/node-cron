dfbf30c843e3c57d175944a24f31318f48e7f768
Wendler <ramoswendler@gmail.com>
2022-12-02 14:39:02 -0300

    Updating cron route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import cron from 'node-cron';
+import axios from 'axios';
+
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+  console.log(`Running cron scheduller`);
+  try {
+    await axios.get('https://controle-de-gastos-api-production.up.railway.app/scheduler')
+  }catch(e){ 
+    console.log(`Error: {e}`)
+  }
 });
